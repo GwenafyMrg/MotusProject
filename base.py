b675de7efceac7c7eba64be49ca129fad0e72874
Gwenafy <gwenafy@gmail.com>
2024-07-24 11:21:17 +0200

    base.py --> create comparison function in order to update the hiding word with the different try of player.

--- base.py
+++ base.py
@@ -43,20 +43,46 @@ def correct(word):
     for letter in word:
         carac = ord(letter);
         if (carac < 97 or carac > 122):
+            print("Votre proposition ne doit contenir que des lettres et pas d'autres caractères...")
             return False;
     return True;
 
-words = ["Maison"];
-words_to_find = words[0];
-hiding_word = hide_word(words_to_find);
-user_word = demand_answer();
-letters = [];
-tries = 0;
+def compare_word(proposition, word_to_find, hiding_word):
+    hiding_word = list(hiding_word);
+    for i in range(len(word_to_find)):
+        if (hiding_word[i] == "X" and proposition[i] == word_to_find[i]):
+            hiding_word[i] = proposition[i];
+            if (proposition[i] not in reveal_letters):
+                reveal_letters.append(proposition[i]);
+        if (hiding_word[i] == "X" and proposition[i] != word_to_find[i]):
+            if (proposition[i] not in wrong_letters):
+                wrong_letters.append(proposition[i]);
+    hiding_word = "".join(hiding_word)
+    print("Voici les lettres que vous avez révélé dans le mot :",hiding_word);
+    print("Lettres révélées :", reveal_letters);
+    print("Mauvaises lettres :",wrong_letters);
+    return hiding_word;
 
+words = ["Maison"];
+word_to_find = words[0];
+hiding_word = hide_word(word_to_find);
+user_word = "";
+reveal_letters = [];
+wrong_letters = [];
+tries = 0;
 
 # print(words_to_find);
 # print(hiding_word);
+# print("\n");
+# print(user_word);
+# user_word = demand_answer();
 # print(user_word);
 
-#35 min
-#18h
+'''Test de la fonction de comparaison de mot manuel.'''
+print("Voici le mot que vous devez révélé :",hiding_word);
+hiding_word = compare_word("marron","maison","XXXXXX");
+print("Voici le mot que vous devez révélé :",hiding_word);
+hiding_word = compare_word("marbre","maXXon","XXXXXX");
+
+#50 min
+#10h40
